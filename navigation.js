f21a07fa613066bdd39b8d8b7058508e03e0c46c
Harsh Kaushal <ggi2022.2693@ggi.ac.in>
2023-09-15 10:22:18 +0530

    let's go

---
+++ navigation.js
@@ -0,0 +1,9 @@
+const close=document.getElementById("close-btn");
+const menu=document.getElementById("menu-btn");
+const navbar=document.getElementById("navbar");
+menu.addEventListener("click",()=>{
+    navbar.style.right="0";
+})
+close.addEventListener("click",()=>{
+    navbar.style.right="-100vw";
+})
